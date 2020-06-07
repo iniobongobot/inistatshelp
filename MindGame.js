006e857d364dfe19f2154dbb7c75efb5d225510d
iniobongobot <63253619+iniobongobot@users.noreply.github.com>
2020-06-07 16:49:12 -0400

    Add files via upload

---
+++ MindGame.js
@@ -0,0 +1,74 @@
+function MindGame(){
+	let Game = document.getElementById('MindGame').value;
+	if (Game == 'RPS'){
+		var output= "<fieldset><legend>Rock ,Paper, Scissors</legend>"+
+				"<select id="+"'rps'"+"><option value="+"'Nothing'"+"selected>Pick One to Play</option>"+
+				"<option value="+"'Rock'"+">Rock</option>"+
+				"<option value="+"'Paper'"+">Paper</option>"+
+				"<option value="+"'Scissors'"+">Scissors</option>"+
+			"</select>"+	
+			"</br>"+
+			"<button onclick="+'RPS()'+">Let's Play</button>"+
+			"</fieldset></br>"+
+			"<p id="+"'answer'"+"></p>";
+	}
+	else if (Game == 'Guess'){
+		console.log('Guessing Game');
+	}
+	else if (Game == 'Magic'){
+		console.log('Magic 8 Ball');
+	}
+	else{
+		var output = "<h2 style="+"'color:red'"+">Pick a Game to play</h2>";
+	}
+	document.getElementById('secassign').innerHTML=output;
+}
+
+function RPS(){
+let rps = document.getElementById('rps').value;
+var random = Math.floor(Math.random() * 3) + 1;
+//1-Rock, 2-Paper, 3-Sciccor
+let one="Rock",	two="Paper", three="Scissors";
+//now comes the if statement
+var result = false, say = false;
+if(rps=="Nothing"){
+	say= "Missing guess parameter";
+}
+else if (random==1 && rps == one){
+	result = "DRAW!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+one+"</br><hr> Thank you for Playing!!</h2>";
+}
+else if (random==1 && rps == two){
+	result = "YOU WIN!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+one+"</br><hr> Thank you for Playing!!</h2>";
+}
+ else if (random==1 && rps == three){
+	result = "YOU LOSE!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+one+"</br><hr> Thank you for Playing!!</h2>";
+}
+else if (random==2 && rps == one){
+	result = "YOU LOSE!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+two+"</br><hr> Thank you for Playing!!</h2>";
+}
+else if (random==2 && rps == two){
+	result = "DRAW!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+two+"</br><hr> Thank you for Playing!!</h2>";
+}
+ else if (random==2 && rps == three){
+	result = "YOU WIN!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+two+"</br><hr> Thank you for Playing!!</h2>";
+}
+else if (random==3 && rps == one){
+	result = "YOU WIN!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+three+"</br><hr> Thank you for Playing!!</h2>";
+}
+else if (random==3 && rps == two){
+	result = "YOU LOSE!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+three+"</br><hr> Thank you for Playing!!</h2>";
+}
+ else if (random==3 && rps == three){
+	result = "DRAW!!";
+	say="<h1>"+result+"</h1><h2>You Chose: "+rps+", Computer Chose "+three+"</br><hr> Thank you for Playing!!</h2>";
+}
+document.getElementById('answer').innerHTML=say;
+}
